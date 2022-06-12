bea702c540085a62c9e7f61482d00c9d280b0992
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-06-12 18:25:18 +0530

    Updates

---
+++ precache-manifest.2b324a0ffa2fe35554488d974d929fba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cf2c28c5ba6b1fb77410f9ec3f2ecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0522e6bf909749c999be",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6e2ecbbb92201f932a4e",
+    "url": "/static/css/main.7785a27a.chunk.css"
+  },
+  {
+    "revision": "0522e6bf909749c999be",
+    "url": "/static/js/2.293015fe.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.293015fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2ecbbb92201f932a4e",
+    "url": "/static/js/main.56cc7168.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
